bb05004e521d9e3892f0c20ba28e1cb651bba8af
Kindle <126899882+kindlesz@users.noreply.github.com>
2023-03-16 17:56:33 -0300

    feat: Banner

---
+++ src/Componentes/Colaborador/index.js
@@ -0,0 +1,14 @@
+import './/Colaborador.css'
+const Colaborador = ({ nome, imagem, cargo, corDeFundo }) => {
+    return (<div className='Colaborador'>
+        <div className='cabecalho' style={{ backgroundColor: corDeFundo }}>
+            <img src={imagem} alt={nome}/>
+            </div>
+            <div className='rodape'>
+                <h4>{nome}</h4>
+                <h5>{cargo}</h5>
+        </div>
+    </div>)
+}
+
+export default Colaborador
